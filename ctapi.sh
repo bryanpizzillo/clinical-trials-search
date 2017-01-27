30cf64e739dbdb04e8b9352cbb25be1937ae24d1
Bryan Pizzillo <bryanp@mail.nih.gov>
2017-01-27 16:18:16 -0500

    Set config to use Elasticsearch name for net. docker-compose uses container name as network host.  so added elasticsearch as the search host.

--- ctapi.sh
+++ ctapi.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+_base="$(dirname $_script)"
+
+
 function usage
 {
     echo "usage: ctapi.sh <program>"
@@ -10,9 +14,46 @@ function usage
 }
 
 function run_indexer
-{
-    echo "indexer"
+{    
+
+    set_config
     
+    echo "Running Indexer..."
+
+    ## Make sure we are in the app root.    
+    cd $_base
+
+    ## Move into the transformer
+    pushd ./import/transform
+
+    ## Run transformer
+    npm run transform-trials
+    transform_exitc=$?
+    if [ $transform_exitc -ne 0 ]; then
+        echo "Transformer Failed. Exiting..."
+        exit $transform_exitc
+    fi
+
+    ## Go back to the main app folder.
+    popd
+
+    ## Go to indexer.
+    pushd ./search/index
+
+    ## Run indexer
+    npm run index
+    indexer_exitc=$?
+    if [ $indexer_exitc -ne 0 ]; then
+        echo "Indexer Failed. Exiting..."
+        exit $indexer_exitc
+    fi    
+
+    # Backup to main app folder
+    popd
+
+    echo "Indexer Completed..."
+
+    exit 0
 }
 
 function run_api
